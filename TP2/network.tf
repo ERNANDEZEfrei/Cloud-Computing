b89ba72676b02f3cf26f6ce83e33e43a94c6654a
ErnandezAlexandre <79079150+Shazoow@users.noreply.github.com>
2025-09-09 19:11:43 +0200

    Mouve NSG to network.tf

--- TP2/network.tf
+++ TP2/network.tf
@@ -1,1 +1,17 @@
- 
+resource "azurerm_network_security_group" "main" {
+  name                = "VMTP2Terraform-nsg"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "Allow_SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = var.my_ip
+    destination_address_prefix = "*"
+  }
+}
